504b9ddc74c4840353869ad91ca9cf0691e98e7b
eccpluto <alleyes.payday591@passmail.net>
2024-06-10 03:10:18 +1200

    basic account page done

---
+++ material-ui-vite/src/pages/AccountPage.jsx
@@ -0,0 +1,54 @@
+import Copyright from "../components/Copyright"
+import ProTip from "../components/ProTip"
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import Box from "@mui/material/Box"
+import useTheme from "@mui/material/styles/useTheme"
+import { useUserContext } from "../context/UserContext"
+
+function AccountPage(props) {
+    const theme = useTheme();
+    const { user, handleUpdateUser } = useUserContext();
+
+    if (!user.email) {
+        return (
+            <Container maxWidth="xl">
+                <Box sx={{
+                    my: 4,
+                    // bgcolor:    theme.palette.secondary.main,
+                    borderRadius: 4,
+                }}>
+                    <Typography>
+                        You have been logged out. Please leave this page.
+                    </Typography>
+                </Box>
+            </Container >
+        )
+    }
+
+    return (
+        <Container maxWidth="xl">
+            <Box sx={{
+                my: 4,
+                // bgcolor:    theme.palette.secondary.main,
+                borderRadius: 4,
+            }}>
+                <Typography variant="h4" component="h1" sx={{
+                    mb: 2,
+                    color: theme.palette.primary.main,
+                }}>
+                    Manage your account
+                </Typography>
+                <Typography>
+                    Welcome, {user.email}!
+                </Typography>
+                <button onClick={() => {
+                    handleUpdateUser({});
+
+                }}>Log Out</button>
+            </Box>
+        </Container >
+    )
+}
+
+export default AccountPage
--- material-ui-vite/src/routes/AppRoutes.jsx
+++ material-ui-vite/src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import AccountPage from "../pages/AccountPage";
 
 function AppRoutes(props) {
     return (
         <Routes>
             <Route index element={<HomePage {...props} />} />
             <Route path="/login" element={<LoginPage {...props} />} />
+            <Route path="/account" element={<AccountPage {...props} />} />
         </Routes>
     )
 }
