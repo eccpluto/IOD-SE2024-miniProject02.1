a256864e358245e1be09fc83626c79fcf629cdbf
eccpluto <alleyes.payday591@passmail.net>
2024-06-10 03:37:37 +1200

    added library table template code, will need to store some data / pointers to data in here.

--- material-ui-vite/src/pages/LibraryPage.jsx
+++ material-ui-vite/src/pages/LibraryPage.jsx
@@ -4,6 +4,7 @@ import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
 import useTheme from "@mui/material/styles/useTheme"
+import VirtualisedTable from "../components/VirtualisedTable"
 
 function LibraryPage(props) {
     const theme = useTheme();
@@ -20,6 +21,7 @@ function LibraryPage(props) {
                 }}>
                     Library
                 </Typography>
+                <VirtualisedTable></VirtualisedTable>
                 <ProTip />
                 <Copyright />
             </Box>
